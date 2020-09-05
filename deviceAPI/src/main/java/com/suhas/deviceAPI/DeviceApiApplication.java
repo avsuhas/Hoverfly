36bf5f88ba71e86765776419dc3ebb6a4541055d
Suhas V Arudi <avsuhas@gmail.com>
2020-09-05 13:15:33 -0400

    Device API - To demonstrate purpose of hoverfly

---
+++ deviceAPI/src/main/java/com/suhas/deviceAPI/DeviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.suhas.deviceAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceApiApplication.class, args);
+	}
+
+}
---
+++ deviceAPI/src/test/java/com/suhas/deviceAPI/DeviceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhas.deviceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
